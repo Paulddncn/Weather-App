c903303ee5375c6bc4d5058b25005c625fbd1c0b
Paul Duncan <paulddncn@gmail.com>
2023-04-05 17:06:12 -0600

    regrade commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,46 +1,41 @@
-
-//GIVEN a weather dashboard with form inputs
-//WHEN I search for a city
-//THEN I am presented with current and future conditions for that city and that city is added to the search history
-//WHEN I view current weather conditions for that city
-//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-//WHEN I view future weather conditions for that city
-//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-//WHEN I click on a city in the search history
-//THEN I am again presented with current and future conditions for that city
-
-
-var weatherIcon = ''
 var searchBtn = document.querySelector(".search-btn")
-var forecastTemp = document.querySelector(".forecast-temp")
-var temperature = document.querySelector("#temp")
-var wind = document.querySelector("#wind")
-var humidity = document.querySelector(".forecast-humidity")
-var uvindex = document.querySelector("#uvindex")
-var weatherIcon = document.querySelector("#weather-icon")
-var fiveDayForecast = document.querySelector("#five-day-forecast")
 var searchInput = document.querySelector("#search-input")
-var citySearch = searchInput.value
 var key = '11a69b2b8d9505e35af4031a2fe1c8ca'
 
+var currentCity = document.querySelector("#current-city")
+var date = document.querySelector("#date")
+var weatherIcon = document.querySelector("#weather-icon")
+var temp = document.querySelector("#temp")
+var wind = document.querySelector("#wind")
+var humid = document.querySelector("#humidity")
+
+var forecastDate = document.querySelectorAll(".forecast-date")
+var forecastIcon = document.querySelectorAll(".icon")
+var forecastTemp = document.querySelectorAll(".forecast-temp")
+var forecastWind = document.querySelectorAll(".forecast-wind")
+var forecastHumidity = document.querySelectorAll(".forecast-humidity")
+//  {Date} dateObject
+// function formatDate(dateObject) {
+//   const parts = {
+//     date: dateObject.getDate(),
+//     month: dateObject.getMonth() + 1,
+//     year: dateObject.getFullYear()
+//   
 
 
-//var today = dayjs();
-//$('#date').text(today.format('MMM D, YYYY'));
 
-
-//var today = dayjs().format('MMM D, YYYY');
-//$('#date').text(today);
-
-
+// var today = dayjs().format('MMM D, YYYY');
+// $('#date').text(today);
 
 searchBtn.addEventListener("click", function () {
-  getGeoLocation(citySearch);
+
+  getGeoLocation();
 })
 
 
 var getGeoLocation = function () {
-  var geoCoding = 'http://api.openweathermap.org/geo/1.0/direct?q=Denver&appid=' + key
+  var citySearch = searchInput.value
+  var geoCoding = `http://api.openweathermap.org/geo/1.0/direct?q=${citySearch}&appid=` + key
   fetch(geoCoding)
     .then(function (response) {
       return response.json();
@@ -48,41 +43,82 @@ var getGeoLocation = function () {
     .then(function (data) {
       console.log(data[0].lat, data[0].lon);
       getCurrentWeather(data[0].lat, data[0].lon);
+      getWeatherForecast(data[0].lat, data[0].lon);
     })
 };
 
 var getCurrentWeather = function (lat, lon) {
-  var fiveDayForecast = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + key;
+  var currentWeather = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + key + "&units=imperial";
+  fetch(currentWeather)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+     
+      console.log(data)
+      
+
+      temperature = (data.main.temp)
+      windSpeed = (data.wind.speed)
+      humid = (data.main.humidity)
+      icon = (data.weather[0].icon)
+      console.log(icon)
+      var today = dayjs();
+
+      date.textContent = today.format('MMM D, YYYY');
+      currentCity.textContent = "City: " + searchInput.value
+      weatherIcon.textContent = "http:openweathermap.org/img/wn/" + icon + ".png";
+      temp.textContent = "Temp: " + temperature + "°"
+      wind.textContent = "windspeed: " + windSpeed + "mph"
+      humidity.textContent = "Humidity: " + humid + "%"
+      
+    })
+}
+
+var getWeatherForecast = function (lat, lon) {
+  var fiveDayForecast = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + key + "&units=imperial";
+  
   fetch(fiveDayForecast)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.list[0].wind) 
+      var today = dayjs();
       console.log(data)
-      var temp = data.list[0].main.temp
-      var humid = data.list[0].main.humidity
-      console.log(temp)
-
+    
       for (let index = 0; index < data.list.length; index = index + 8) {
-          humidity = (data.list[index].main.humidity)
-          temp = (data.list[index].main.temp)
-          temp = ((temp-273.15)*1.+32).toFixed()
+        var humid = (data.list[index].main.humidity)
+        var windSpeed = (data.list[index].wind.speed)
+        var temperature = (data.list[index].main.temp)
+        var day = today.add(index/8 + 1, 'day')
+        console.log(index)
 
-          forecastTemp.textContent = "temperaure: " + temp + "°"
-          humidity.textContent = "humidity: " + humid
-          console.log(humid)
+      
+        //currentCity.textContent = "City: " + searchInput.value
 
-      }})}
-         
-
-
-
-
-
-
+        //forecastIcon[index/8].textContent = "http:openweathermap.org/img/wn/" + icons + ".png"
+        forecastDate[index / 8].textContent = day.format('MMM D, YYYY');
+        forecastTemp[index / 8].textContent = "Temp: " + temperature + "°"
+        forecastWind[index / 8].innerHTML = "Windspeed: " + windSpeed + "mph";
+        forecastHumidity[index / 8].textContent = "Humidity: " + humid + "%";
 
 
 
 
- 
+      }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
